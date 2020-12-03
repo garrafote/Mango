07df675904e62ab65289a9b8f3f308310c2eb4e9
Lucas Pereira Vasconcelos <garrafote@gmail.com>
2020-12-03 10:23:44 +0000

    Logging

--- Mango/src/Mango/EntryPoint.h
+++ Mango/src/Mango/EntryPoint.h
@@ -7,6 +7,10 @@ extern Mango::Application* Mango::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Mango::Log::Init();
+	MGO_CORE_TRACE("Initialized Core Logger");
+	MGO_INFO("Welcome to Mango, {0}!", 42);
+
 	Mango::Application* app = Mango::CreateApplication();
 	app->Run();
 	delete app;
---
+++ Mango/src/Mango/Log.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <memory>
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Mango {
+
+	class MANGO_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
+
+// Core log macros
+
+#define MGO_CORE_TRACE(...)   ::Mango::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define MGO_CORE_INFO(...)    ::Mango::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define MGO_CORE_WARN(...)    ::Mango::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define MGO_CORE_ERROR(...)   ::Mango::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define MGO_CORE_FATAL(...)   ::Mango::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+// Core log macros
+
+#define MGO_TRACE(...)   ::Mango::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define MGO_INFO(...)    ::Mango::Log::GetClientLogger()->info(__VA_ARGS__)
+#define MGO_WARN(...)    ::Mango::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define MGO_ERROR(...)   ::Mango::Log::GetClientLogger()->error(__VA_ARGS__)
+#define MGO_FATAL(...)   ::Mango::Log::GetClientLogger()->fatal(__VA_ARGS__)

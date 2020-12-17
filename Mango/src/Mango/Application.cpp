54d43336a02f20ca1c978d76752c23d66628c8ca
Lucas Pereira Vasconcelos <garrafote@gmail.com>
2020-12-17 11:33:34 +0000

    Shader abstraction

--- Mango/src/Mango/Application.cpp
+++ Mango/src/Mango/Application.cpp
@@ -63,7 +63,7 @@ namespace Mango {
 			}
 		)";
 
-		m_Shader.reset(new Shader(vertexSrc, fragmentSrc));
+		m_Shader.reset(Shader::Create(vertexSrc, fragmentSrc));
 	}
 
 	Application::~Application()

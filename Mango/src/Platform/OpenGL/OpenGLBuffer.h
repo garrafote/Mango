9d786f361de819d4c0b37288e33ce6394c836ff4
Lucas Pereira Vasconcelos <garrafote@gmail.com>
2020-12-18 10:46:29 +0000

    Vertex Buffer Layouts

--- Mango/src/Platform/OpenGL/OpenGLBuffer.h
+++ Mango/src/Platform/OpenGL/OpenGLBuffer.h
@@ -10,11 +10,15 @@ namespace Mango {
 		OpenGLVertexBuffer(float* vertices, uint32_t size);
 		virtual ~OpenGLVertexBuffer();
 
+		virtual const BufferLayout& GetLayout() const override { return m_Layout; };
+		virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; };
+
 		virtual void Bind() const override;
 		virtual void Unbind() const override;
 
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer

0758cec3c74f955872f0f8a046477ff93ab1ed56
Lucas Pereira Vasconcelos <garrafote@gmail.com>
2020-12-28 10:35:52 +0000

    Refs, Scopes and Smart Pointers

--- Mango/src/Mango/Core.h
+++ Mango/src/Mango/Core.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <memory>
 
 #ifdef MANGO_PLATFORM_WINDOWS
 	#if MANGO_DYNAMIC_LINK
@@ -29,3 +30,14 @@
 #define BIT(x) (1 << (x))
 
 #define MGO_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+
+
+namespace Mango {
+
+	template<typename T>
+	using Scope = std::unique_ptr<T>;
+
+
+	template<typename T>
+	using Ref = std::shared_ptr<T>;
+}

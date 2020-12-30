b4f9eb903c5e5b784935d43fb1ccb717630d1f8f
Lucas Pereira Vasconcelos <garrafote@gmail.com>
2020-12-30 10:34:43 +0000

    Resizing

--- Mango/src/Mango/Application.cpp
+++ Mango/src/Mango/Application.cpp
@@ -10,7 +10,7 @@ namespace Mango {
 	Application* Application::s_Instance = nullptr;
 
 	Application::Application()
-			{
+	{
 		MGO_CORE_ASSERT(!s_Instance, "Application already exists!");
 		s_Instance = this;
 
@@ -43,6 +43,7 @@ namespace Mango {
 	{
 		EventDispatcher	dispatcher(e);
 		dispatcher.Dispatch<WindowCloseEvent>(MGO_BIND_EVENT_FN(Application::OnWindowClose));
+		dispatcher.Dispatch<WindowResizeEvent>(MGO_BIND_EVENT_FN(Application::OnWindowResize));
 
 		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
 		{
@@ -60,8 +61,11 @@ namespace Mango {
 			Timestep timestep = time - m_LastFrameTime;
 			m_LastFrameTime = time;
 
-			for (Layer* layer : m_LayerStack)
-				layer->OnUpdate(timestep);
+			if (!m_Minimized)
+			{
+				for (Layer* layer : m_LayerStack)
+					layer->OnUpdate(timestep);
+			}
 
 			m_ImGuiLayer->Begin();
 			for (Layer* layer : m_LayerStack)
@@ -76,6 +80,20 @@ namespace Mango {
 	{
 		m_Running = false;
 		return true;
+	}
+
+	bool Application::OnWindowResize(WindowResizeEvent& e)
+	{
+		if (e.GetWidth() == 0 || e.GetHeight() == 0)
+		{
+			m_Minimized = true;
+			return false;
+		}
+
+		m_Minimized = false;
+		Renderer::OnWindowResize(e.GetWidth(), e.GetHeight());
+
+		return false;
 	}
 }
 

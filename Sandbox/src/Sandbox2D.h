3b1e767257d4cda7b0367114c026487903e352c2
Lucas Pereira Vasconcelos <garrafote@gmail.com>
2020-12-31 16:25:04 +0000

    Intro to Profiling

--- Sandbox/src/Sandbox2D.h
+++ Sandbox/src/Sandbox2D.h
@@ -20,6 +20,14 @@ private:
 
 	Mango::Ref<Mango::Texture2D> m_Texture;
 
+	struct ProfileResult
+	{
+		const char* Name;
+		float Time;
+	};
+
+	std::vector<ProfileResult> m_ProfileResults;
+
 	glm::vec4 m_SquareColor = { 0.8f, 0.2f, 0.3f, 0.8f };
 };
 

6a867b1ffe0fb470b6cf020c21ae36ee7e638921
Lucas Pereira Vasconcelos <garrafote@gmail.com>
2021-01-11 11:31:06 +0000

    Fixed incorrect cases in VertexBuffer layout
    Fixed incorrect cases in VertexBuffer layout

--- Mango/src/Platform/OpenGL/OpenGLVertexArray.cpp
+++ Mango/src/Platform/OpenGL/OpenGLVertexArray.cpp
@@ -4,22 +4,22 @@
 #include <glad/glad.h>
 
 namespace Mango {
-	
+
 	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
 	{
 		switch (type)
 		{
-			case ShaderDataType::Float:   return GL_FLOAT;
-			case ShaderDataType::Float2:  return GL_FLOAT;
-			case ShaderDataType::Float3:  return GL_FLOAT;
-			case ShaderDataType::Float4:  return GL_FLOAT;
-			case ShaderDataType::Mat3:    return GL_FLOAT;
-			case ShaderDataType::Mat4:    return GL_FLOAT;
-			case ShaderDataType::Int:     return GL_INT;
-			case ShaderDataType::Int2:    return GL_INT;
-			case ShaderDataType::Int3:    return GL_INT;
-			case ShaderDataType::Int4:    return GL_INT;
-			case ShaderDataType::Bool:    return GL_BOOL;
+		case ShaderDataType::Float:   return GL_FLOAT;
+		case ShaderDataType::Float2:  return GL_FLOAT;
+		case ShaderDataType::Float3:  return GL_FLOAT;
+		case ShaderDataType::Float4:  return GL_FLOAT;
+		case ShaderDataType::Mat3:    return GL_FLOAT;
+		case ShaderDataType::Mat4:    return GL_FLOAT;
+		case ShaderDataType::Int:     return GL_INT;
+		case ShaderDataType::Int2:    return GL_INT;
+		case ShaderDataType::Int3:    return GL_INT;
+		case ShaderDataType::Int4:    return GL_INT;
+		case ShaderDataType::Bool:    return GL_BOOL;
 		}
 
 		MGO_CORE_ASSERT(false, "Unknown ShaderDataType!");
@@ -32,21 +32,21 @@ namespace Mango {
 
 		glGenVertexArrays(1, &m_RendererID);
 	}
-	
+
 	OpenGLVertexArray::~OpenGLVertexArray()
 	{
 		MGO_PROFILE_FUNCTION();
 
 		glDeleteVertexArrays(1, &m_RendererID);
 	}
-	
+
 	void OpenGLVertexArray::Bind() const
 	{
 		MGO_PROFILE_FUNCTION();
 
 		glBindVertexArray(m_RendererID);
 	}
-	
+
 	void OpenGLVertexArray::Unbind() const
 	{
 		MGO_PROFILE_FUNCTION();
@@ -59,27 +59,68 @@ namespace Mango {
 		MGO_PROFILE_FUNCTION();
 
 		MGO_CORE_ASSERT(vertexBuffer->GetLayout().GetElements().size(), "Vertex Buffer has no layout!");
-		
+
 		glBindVertexArray(m_RendererID);
 		vertexBuffer->Bind();
 
-		uint32_t index = 0;
 		const auto& layout = vertexBuffer->GetLayout();
 		for (const auto& element : layout)
 		{
-			glEnableVertexAttribArray(m_VertexBufferIndex);
-			glVertexAttribPointer(m_VertexBufferIndex,
-				element.GetComponentCount(), 
-				ShaderDataTypeToOpenGLBaseType(element.Type),
-				element.Normalized ? GL_TRUE : GL_FALSE,
-				layout.GetStride(),
-				(const void*)element.Offset);
-			m_VertexBufferIndex++;
+			uint8_t count = element.GetComponentCount();
+			switch (element.Type)
+			{
+			case ShaderDataType::Float:
+			case ShaderDataType::Float2:
+			case ShaderDataType::Float3:
+			case ShaderDataType::Float4:
+
+				glEnableVertexAttribArray(m_VertexBufferIndex);
+				glVertexAttribPointer(m_VertexBufferIndex,
+					count,
+					ShaderDataTypeToOpenGLBaseType(element.Type),
+					element.Normalized ? GL_TRUE : GL_FALSE,
+					layout.GetStride(),
+					(const void*)element.Offset);
+				m_VertexBufferIndex++;
+				break;
+
+			case ShaderDataType::Mat3:
+			case ShaderDataType::Mat4:
+
+				for (uint8_t index = 0; index < count; index++)
+				{
+					glEnableVertexAttribArray(m_VertexBufferIndex);
+					glVertexAttribPointer(m_VertexBufferIndex,
+						count,
+						ShaderDataTypeToOpenGLBaseType(element.Type),
+						element.Normalized ? GL_TRUE : GL_FALSE,
+						layout.GetStride(),
+						(const void*)(element.Offset + index * element.Size / count));
+					glVertexAttribDivisor(m_VertexBufferIndex, 1);
+					m_VertexBufferIndex++;
+				}
+				break;
+
+			case ShaderDataType::Int:
+			case ShaderDataType::Int2:
+			case ShaderDataType::Int3:
+			case ShaderDataType::Int4:
+			case ShaderDataType::Bool:
+				
+				glEnableVertexAttribArray(m_VertexBufferIndex);
+				glVertexAttribIPointer(m_VertexBufferIndex,
+					count,
+					ShaderDataTypeToOpenGLBaseType(element.Type),
+					layout.GetStride(),
+					(const void*)element.Offset);
+				m_VertexBufferIndex++;
+				break;
+			}
 		}
 
 		m_VertexBuffers.push_back(vertexBuffer);
 	}
-	
+
 	void OpenGLVertexArray::SetIndexBuffer(const Ref<IndexBuffer>& indexBuffer)
 	{
 		MGO_PROFILE_FUNCTION();
--- Sandbox/assets/shaders/Unlit2D.glsl
+++ Sandbox/assets/shaders/Unlit2D.glsl
@@ -6,7 +6,7 @@
 layout(location = 0) in vec3 a_Position;
 layout(location = 1) in vec4 a_Color;
 layout(location = 2) in vec2 a_TexCoord;
-layout(location = 3) in float a_TexIndex;
+layout(location = 3) in int a_TexIndex;
 layout(location = 4) in vec4 a_Tiling;
 
 uniform mat4 u_ViewProjection;
@@ -21,7 +21,7 @@ void main()
 {
 	v_Color = a_Color;
 	v_TexCoord = a_TexCoord;
-	v_TexIndex = int(a_TexIndex);
+	v_TexIndex = a_TexIndex;
 	v_Tiling = a_Tiling;
 	gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
 }

536c531ea11e23ea839fc1590cea0ec4592a498a
Lucas Pereira Vasconcelos <garrafote@gmail.com>
2021-01-11 11:42:44 +0000

    constexpr for Function Names on Instrumentor

--- Mango/src/Mango/Debug/Instrumentor.h
+++ Mango/src/Mango/Debug/Instrumentor.h
@@ -9,11 +9,13 @@
 
 namespace Mango {
 	
+
 	using FloatingPointMicroseconds = std::chrono::duration<double, std::micro>;
 
 	struct ProfileResult
 	{
 		std::string Name;
+
 		FloatingPointMicroseconds Start;
 		std::chrono::microseconds ElapsedTime;
 		std::thread::id ThreadID;
@@ -39,23 +41,29 @@ namespace Mango {
 		void BeginSession(const std::string& name, const std::string& filepath = "results.json")
 		{
 			std::lock_guard lock(m_Mutex);
-			if (m_CurrentSession) {
+			if (m_CurrentSession)
+			{
 				// If there is already a current session, then close it before beginning new one.
 				// Subsequent profiling output meant for the original session will end up in the
 				// newly opened session instead.  That's better than having badly formatted
 				// profiling output.
-				if (Log::GetCoreLogger()) { // Edge case: BeginSession() might be before Log::Init()
+				if (Log::GetCoreLogger()) // Edge case: BeginSession() might be before Log::Init()
+				{
 					MGO_CORE_ERROR("Instrumentor::BeginSession('{0}') when session '{1}' already open.", name, m_CurrentSession->Name);
 				}
 				InternalEndSession();
 			}
 			m_OutputStream.open(filepath);
-			if (m_OutputStream.is_open()) {
+
+			if (m_OutputStream.is_open())
+			{
 				m_CurrentSession = new InstrumentationSession({ name });
 				WriteHeader();
 			}
-			else {
-				if (Log::GetCoreLogger()) { // Edge case: BeginSession() might be before Log::Init()
+			else
+			{
+				if (Log::GetCoreLogger()) // Edge case: BeginSession() might be before Log::Init()
+				{
 					MGO_CORE_ERROR("Instrumentor could not open results file '{0}'.", filepath);
 				}
 			}
@@ -71,13 +79,11 @@ namespace Mango {
 		{
 			std::stringstream json;
 
-			std::string name = result.Name;
-			std::replace(name.begin(), name.end(), '"', '\'');
-
+			json << std::setprecision(3) << std::fixed;
 			json << ",{";
 			json << "\"cat\":\"function\",";
 			json << "\"dur\":" << (result.ElapsedTime.count()) << ',';
-			json << "\"name\":\"" << name << "\",";
+			json << "\"name\":\"" << result.Name << "\",";
 			json << "\"ph\":\"X\",";
 			json << "\"pid\":0,";
 			json << "\"tid\":" << result.ThreadID << ",";
@@ -85,13 +91,15 @@ namespace Mango {
 			json << "}";
 
 			std::lock_guard lock(m_Mutex);
-			if (m_CurrentSession) {
+			if (m_CurrentSession)
+			{
 				m_OutputStream << json.str();
 				m_OutputStream.flush();
 			}
 		}
 
-		static Instrumentor& Get() {
+		static Instrumentor& Get()
+		{
 			static Instrumentor instance;
 			return instance;
 		}
@@ -112,8 +120,10 @@ namespace Mango {
 
 		// Note: you must already own lock on m_Mutex before
 		// calling InternalEndSession()
-		void InternalEndSession() {
-			if (m_CurrentSession) {
+		void InternalEndSession()
+		{
+			if (m_CurrentSession)
+			{
 				WriteFooter();
 				m_OutputStream.close();
 				delete m_CurrentSession;
@@ -150,9 +160,38 @@ namespace Mango {
 		}
 	private:
 		const char* m_Name;
-		std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimepoint;
+		std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
 		bool m_Stopped;
 	};
+
+	namespace InstrumentorUtils {
+
+		template <size_t N>
+		struct ChangeResult
+		{
+			char Data[N];
+		};
+
+		template <size_t N, size_t K>
+		constexpr auto CleanupOutputString(const char(&expr)[N], const char(&remove)[K])
+		{
+			ChangeResult<N> result = {};
+
+			size_t srcIndex = 0;
+			size_t dstIndex = 0;
+			while (srcIndex < N)
+			{
+				size_t matchIndex = 0;
+				while (matchIndex < K - 1 && srcIndex + matchIndex < N - 1 && expr[srcIndex + matchIndex] == remove[matchIndex])
+					matchIndex++;
+				if (matchIndex == K - 1)
+					srcIndex += matchIndex;
+				result.Data[dstIndex++] = expr[srcIndex] == '"' ? '\'' : expr[srcIndex];
+				srcIndex++;
+			}
+			return result;
+		}
+	}
 }
 
 #define MANGO_PROFILE 0
@@ -164,7 +203,7 @@ namespace Mango {
 		#define __MGO_FUNCSIG__ __PRETTY_FUNCTION__
 	#elif defined(__DMC__) && (__DMC__ >= 0x810)
 		#define __MGO_FUNCSIG__ __PRETTY_FUNCTION__
-	#elif defined(__FUNCSIG__)
+	#elif (defined(__FUNCSIG__) || (_MSC_VER))
 		#define __MGO_FUNCSIG__ __FUNCSIG__
 	#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
 		#define __MGO_FUNCSIG__ __FUNCTION__
@@ -183,7 +222,8 @@ namespace Mango {
 
 	#define MGO_PROFILE_BEGIN_SESSION(name, filepath) ::Mango::Instrumentor::Get().BeginSession(name, filepath)
 	#define MGO_PROFILE_END_SESSION() ::Mango::Instrumentor::Get().EndSession() 
-	#define MGO_PROFILE_SCOPE(name) ::Mango::InstrumentationTimer __MGO_TOKEN_EVALUATE__(__timer,__COUNTER__)(name);
+	#define MGO_PROFILE_SCOPE(name) constexpr auto fixedName = ::Mango::InstrumentorUtils::CleanupOutputString(name, "__cdecl ");\
+									::Mango::InstrumentationTimer __MGO_TOKEN_EVALUATE__(__timer,__COUNTER__)(fixedName.Data)
 	#define MGO_PROFILE_FUNCTION() MGO_PROFILE_SCOPE(__MGO_FUNCSIG__)
 #else
 	#define MGO_PROFILE_BEGIN_SESSION(name, filepath)
